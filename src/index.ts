678f523b2920114b479a40e9bd4bde7a04c99b40
Ayush Chowdhury <ayush.chowdhury2004@gmail.com>
2021-01-09 16:49:46 +0530

    body parser

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
     return res.json({ success: true });
